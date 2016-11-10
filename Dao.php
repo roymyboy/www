8a98d4e71e51440b830ceec9e72d21043bd17df1
Utsav Roy <utroy@localhost.localdomain>
2016-11-10 11:04:15 -0700

    things

--- Dao.php
+++ Dao.php
@@ -1,5 +1,4 @@
 <?php
-	require_once "index.php";
 	class Dao{
 		private $host = "us-cdbr-iron-east-04.cleardb.net";
 		private $db   = "heroku_c47f3897b8b9090";
@@ -7,7 +6,7 @@
 		private $pwd  = "3abc0052";
 	
 		public function getConnection(){
-		     return new PDO("mysql:host={$this->host};dbname={$this->db}";$this->user; 					$this->pwd;);
+		     return new PDO("mysql:host={$this->host};dbname={$this->db}";$user; 					$pwd;);
 		}	
 	
 	}
