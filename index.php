dc4dfb2aa815afc15b8773192de165f0245f00e0
Utsav Roy <utroy@localhost.localdomain>
2016-11-10 10:35:15 -0700

    things

--- index.php
+++ index.php
@@ -1,74 +1,28 @@
 <?php
- 
-//login.php
-require 'index.html';
-/**
- * Start the session.
- */
-//session_start();
- 
-/**
- * Include ircmaxell's password_compat library.
- */
-require 'password.php';
- 
-/**
- * Include our MySQL connection.
- */
-require 'Dao.php';
- 
-//require 'index.html';
- 
-//If the POST var "login" exists (our submit button), then we can
-//assume that the user has submitted the login form.
-if(isset($_POST['login'])){
-    
-    //Retrieve the field values from our login form.
-    $username = !empty($_POST['username@email.com']) ? trim($_POST['username@email.com']) : null;
-    $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
-    
-    //Retrieve the user account information for the given username.
-    $sql = "SELECT id, username@email.com, password FROM users WHERE username = :username@email.com";
-    $stmt = $pdo->prepare($sql);
-    
-    //Bind value.
-    $stmt->bindValue(':username@email.com', $username);
-    
-    //Execute.
-    $stmt->execute();
-    
-    //Fetch row.
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    //If $row is FALSE.
-    if($user === false){
-        //Could not find a user with that username!
-        //PS: You might want to handle this error in a more user-friendly manner!
-        die('Incorrect email / password combination!');
-    } else{
-        //User account found. Check to see if the given password matches the
-        //password hash that we stored in our users table.
-        
-        //Compare the passwords.
-        $validPassword = password_verify($passwordAttempt, $user['password']);
-        
-        //If $validPassword is TRUE, the login has been successful.
-        if($validPassword){
-            
-            //Provide the user with a login session.
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['logged_in'] = time();
-            
-            //Redirect to our protected page, which we called home.php
-            header('Location: home.php');
-            exit;
-            
-        } else{
-            //$validPassword was FALSE. Passwords do not match.
-            die('Incorrect email / password combination!');
-        }
-    }
-    
-}
- 
+require('index.html');
+require('Dao.php');
+session_start();
+// If form submitted, insert values into the database.
+if (isset($_POST['username@email.com'])){
+        // removes backslashes
+	$username = stripslashes($_REQUEST['username@email.com']);
+        //escapes special characters in a string
+	$username = mysqli_real_escape_string($con,$username);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	//Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE username@email.com='$username'
+and password='".md5($password)."'";
+	$result = mysqli_query($con,$query) or die(mysql_error());
+	$rows = mysqli_num_rows($result);
+        if($rows==1){
+	    $_SESSION['username@email.com'] = $username;
+            // Redirect user to index.php
+	    header("Location: index.php");
+         }else{
+	echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+        echo "<p>Not registered yet? <a href='registration.html'>Register Here</a></p>";
+	}
 ?>
