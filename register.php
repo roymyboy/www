1300c4ff2309375d8e5515888a02717567067a3f
Utsav Roy <utroy@localhost.localdomain>
2016-11-15 09:29:32 -0700

    change

--- register.php
+++ register.php
@@ -47,29 +47,6 @@ if (isset($_POST['signup'])) {
     <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
 </head>
 <body>
-
-<nav class="navbar navbar-default" role="navigation">
-    <div class="container-fluid">
-        <!-- add header -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Koding Made Simple</a>
-        </div>
-        <!-- menu items -->
-        <div class="collapse navbar-collapse" id="navbar1">
- <ul class="nav navbar-nav navbar-right">
-                <li><a href="login.php">Login</a></li>
-                <li class="active"><a href="register.php">Sign Up</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
 <div class="container">
     <div class="row">
         <div class="col-md-4 col-md-offset-4 well">
