af236570802d65e8780e075577e0712767e9c9dc
Utsav Roy <utroy@localhost.localdomain>
2016-11-15 07:46:14 -0700

    change

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect('us-cdbr-iron-east-04.cleardb.net', 'bb04ad9bd04b27', '3abc0052', '');
-mysql_select_db('heroku_c47f3897b8b9090');
+mysql_connect('us-cdbr-iron-east-04.cleardb.net', 'bb04ad9bd04b27', '3abc0052', 'heroku_c47f3897b8b9090');
+//mysql_select_db('heroku_c47f3897b8b9090');
 ?>
 
