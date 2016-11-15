91816fcc003173aee4f8ca9c33e9be65430a82fe
Utsav Roy <utroy@localhost.localdomain>
2016-11-15 10:59:20 -0700

    change

--- welcome.php
+++ welcome.php
@@ -9,9 +9,9 @@
    <body>
       <div class="collapse navbar-collapse" id="navbar1">
             <ul class="nav navbar-nav navbar-right">
-                <?php if (isset($_SESSION['usr_id'])) { ?>
+                <?php if (isset($_SESSION['usr_id'])){ ?>
                 <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
-                <li><a href = "logout.php">Sign Out</a>
+                <li><a href = "logout.php">Sign Out</a> <?php } ?>
 	    </li></ul>
       </div>
    </body>
