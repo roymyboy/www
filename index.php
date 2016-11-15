9647dfcf6f156c670fb23442b97f56211c6d4dae
Utsav Roy <utroy@localhost.localdomain>
2016-11-15 10:48:42 -0700

    change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(isset($_SESSION['usr_id'])!="") {
-    header("Location: welcome.php");
+	header("Location: welcome.php");
 }
 
 include_once 'connect.php';
@@ -10,51 +10,52 @@ include_once 'connect.php';
 //check if form is submitted
 if (isset($_POST['login'])) {
 
-    $email = mysqli_real_escape_string($con, $_POST['email']);
-    $password = mysqli_real_escape_string($con, $_POST['password']);
-    $result = mysqli_query($con, "SELECT * FROM users WHERE email = '" . $email. "' and password = '" . md5($password) . "'");
+	$email = mysqli_real_escape_string($con, $_POST['email']);
+	$password = mysqli_real_escape_string($con, $_POST['password']);
+	$result = mysqli_query($con, "SELECT * FROM users WHERE email = '" . $email. "' and password = '" . md5($password) . "'");
 
-    if ($row = mysqli_fetch_array($result)) {
-        $_SESSION['usr_id'] = $row['id'];
-        $_SESSION['usr_name'] = $row['name'];
-        header("Location: welcome.php");
-    } else {
-        $errormsg = "Incorrect Email or Password!!!";
-    }
+	if ($row = mysqli_fetch_array($result)) {
+		$_SESSION['usr_id'] = $row['id'];
+		$_SESSION['usr_name'] = $row['name'];
+		header("Location: welcome.php");
+	} else {
+		$errormsg = "Incorrect Email or Password!!!";
+	}
 }  
 ?>
 
 <!DOCTYPE html>
 <html>
-        <head>
-                <link rel = "stylesheet" type="text/css" href="style.css">
-                <link rel = "icon" type ="image/png" href= "image/favicon.png">
-        </head>
+	<head>
+		<link rel = "stylesheet" type="text/css" href="style.css">
+		<link rel = "icon" type ="image/png" href= "image/favicon.png">
+	</head>
 
-        <!--back ground image-->
-        <div id = "bg">
-         <img src= "image/bg3.jpg" class="stretch" alt = ""/>
-        </div>
+<!--back ground image-->
+	<div id = "bg">
+		<img src= "image/bg3.jpg" class="stretch" alt = ""/>
+	</div>
 
-        <!--Login setup-->
-        <form role="form" action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" name= "loginform"> 
-	  <div class="form-group">
-	  <label for="email"></label>
-          <input type="text" name="email" placeholder="email@email.com" required class="form-control">
-	  </div>	 
-	  <div class="form-group">
-          <label for="password"></label>
-          <input type="password" name="password" placeholder="password" required class="form-control">
-	  </div>
-	  <div class="form-group">
-          <button type="submit" name= "login" value="Login" class="btn btn-primary">login</button> </div>
-          <div class="etc-login-form">
-                <p>forgot your password? <a href="forgotpw.html">click here</a></p>
-                <p>new user? <a href="register.php">create new account</a></p>
-        </div>
-        </form>
+	<!--Login setup-->
+   <div class="Container">	
+	<form role="form" action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" name= "loginform"> 
+		<div class="form-group">
+			<label for="email"></label>
+			<input type="text" name="email" placeholder="email@email.com" required class="form-control">
+		</div>	 
+		<div class="form-group">
+			<label for="password"></label>
+			<input type="password" name="password" placeholder="password" required class="form-control">
+		</div>
+		<div class="form-group">
+			<button type="submit" name= "login" value="Login" class="btn btn-primary">login</button> </div>
+			<div class="etc-login-form">
+			<p>forgot your password? <a href="forgotpw.html">click here</a></p>
+			<p>new user? <a href="register.php">create new account</a></p>
+		</div>
+	</form>
 	<span class="text-danger"><?php if(isset($errormsg)){echo $errormsg;}?></span>
-
+   </div>	
 </html>
 <?php ob_end_flush(); ?>
 ~         
