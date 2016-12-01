ca6d81e84a14da80566c1b4bd2539168be344dab
Utsav Roy <utroy@localhost.localdomain>
2016-12-01 13:08:42 -0700

    bg change

--- forgotpw.php
+++ forgotpw.php
@@ -1,11 +1,29 @@
 <?php
 session_start();
-include_once 'connect.php'; 
+include_once 'connect.php';
+$error = false; 
 if(isset($_POST['submit'])) 
 { 
 $email =  mysqli_real_escape_string($con, $_POST['email']);
 $sql= "SELECT  `password` FROM `users` WHERE `email` ='.$email.'"; 
 $query = mysqli_query($sql); 
+
+  if(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+        $error = true;
+        $email_error = "Please Enter Valid Email ID";
+    } else {
+        //check if user already exists 
+        $query = "SELECT email FROM users WHERE email='$email'";
+        $result = mysqli_query($con, $query);
+        $count = mysqli_num_rows($result);
+                if($count == 0){
+                        $error = true;
+                        $email_error = "Provided email doesnot exist! Please register here <a id="a-color" href='register.php'>Register</a>";
+                }
+    }
+
+
+
 if(!$query)  
     { 
     die(mysqli_error()); 
@@ -53,7 +71,8 @@ else
 					<div class="form-group">
 						<label id="label-text-color" class="email">Email address</label>
 						<input type="email" name="email" placeholder="email@email.com" required class="email">
-					</div>
+						<span class="text-danger"><?php if(isset($email_error))echo $email_error;?></span>	
+				</div>
 				</div>
 				<button id="bgcolor" type="submit" name="submit"  value="submit">Submit</button>
 	</fieldset>
