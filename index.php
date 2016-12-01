93edf876f5cf833b6124efd4249de921564883c9
Utsav Roy <utroy@localhost.localdomain>
2016-12-01 10:05:14 -0700

    bg change

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if (isset($_POST['login'])) {
 		<link rel = "icon" type ="image/png" href= "image/favicon.png">
 	</head>
 <!--back ground image-->
-<div id="bg"><img src= "image/bg3.jpg" class="stretch" alt = ""/></div>
+<div id="bg"><img src= "image/bg5.jpg" class="stretch" alt = ""/></div>
 	<!--Login setup-->
    <div class="Container">	
 	<form role="form" action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" name= "loginform"> 
