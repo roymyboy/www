49e0e3cf8fe685d7838fc0c695c4419ebd70dea5
Utsav Roy <utroy@localhost.localdomain>
2016-12-01 14:57:57 -0700

    bg change

--- forgotpw.php
+++ forgotpw.php
@@ -52,34 +52,26 @@ if(isset($_POST['submit']))
 		<link rel = "stylesheet" type="text/css" href="style.css">
 		 <link rel = "icon" type ="image/png" href= "image/favicon.png">
 	</head>
-	<div id="bg">
-		<img src="image/bg10.jpg" class="stretch" alt=""/>
-	</div>
+	<div id="bg"><img src="image/bg10.jpg" class="stretch" alt=""/></div>
 
 	<div class="container">
-<form role="form" method="post" name="forgotpw" action="<?php echo $_SERVER['PHP_SELF'];?>">
-	<fieldset>
-	<legend id="legendcolor">Forgot Password</legend>
-	<!-- Main Form -->
-	<div class="login-form-1">
-			<div class="etc-login-form">
-				<p id="p-color">When you fill in your registered email address, you will be sent instructions on how to reset your password.</p>
+		<form role="form" method="post" name="forgotpw" action="<?php echo $_SERVER['PHP_SELF'];?>">
+		<fieldset>
+			<legend id="legendcolor">Forgot Password</legend>
+			<!-- Main Form -->
+	     	<div class="login-form-1">
+	 		<div class="etc-login-form"><p id="p-color">When you fill in your registered email address, you will be sent instructions on how to reset your password.</p></div>
+				<div class="form-group">
+					<label for="email">Email address</label>
+					<input type="email" name="email" placeholder="email@email.com" required class="email">
+				</div>
 			</div>
-			<div class="login-form-main-message"></div>
-			<div class="main-login-form">
-				<div class="login-group">
-					<div class="form-group">
-						<label id="label-text-color" class="email">Email address</label>
-						<input type="email" name="email" placeholder="email@email.com" required class="email">
-				</div>
-				</div>
 				<button id="bgcolor" type="submit" name="submit"  value="Forgotw">Submit</button>
-	</fieldset>
-
-				<p id="p-color">Already Registered? <a id="a-color"  href="index.php">login here</a></p>
-				<p id="p-color">New User? <a id="a-color" href="register.php">create new account</a></p>
+		</fieldset>
+			<p id="p-color">Already Registered? <a id="a-color"  href="index.php">login here</a></p>
+			<p id="p-color">New User? <a id="a-color" href="register.php">create new account</a></p>
 		</form>
-	</div>
+		</div>
 	<!-- end:Main Form -->
 	</div>
 
