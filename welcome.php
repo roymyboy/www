987a61e8a737f246fa35ed901fdde5d19e39205c
Utsav Roy <utroy@localhost.localdomain>
2016-12-01 12:09:08 -0700

    bg change

--- welcome.php
+++ welcome.php
@@ -114,30 +114,25 @@ body {font-family: Verdana,sans-serif;margin:0}
 <div class="mySlides fade">
   <div class="numbertext">1 / 5</div>
   <img src="slideshow/s1.jpg" style="width:100%">
-  <div class="text">Caption Text</div>
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">2 / 5</div>
   <img src="slideshow/s2.jpg" style="width:100%">
-  <div class="text">Caption Two</div>
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">3 / 5</div>
   <img src="slideshow/s3.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">4 / 5</div>
   <img src="slideshow/s4.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
 </div>
 <div class="mySlides fade">
   <div class="numbertext">5 / 5</div>
   <img src="slideshow/s5.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
 </div>
 <a class="prev" onclick="plusSlides(-1)">❮</a>
 <a class="next" onclick="plusSlides(1)">❯</a>
