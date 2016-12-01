b00aa5bd693cfac5aa3170df41464bfcc18de4ea
Utsav Roy <utroy@localhost.localdomain>
2016-12-01 13:41:22 -0700

    bg change

--- forgotpw.php
+++ forgotpw.php
@@ -2,20 +2,7 @@
 session_start();
 include_once 'connect.php';
 $error = false; 
- $email = mysqli_real_escape_string($con, $_POST['email']);
-  if(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
-        $error = true;
-        $email_error = "Please Enter Valid Email ID";
-    } else {
-        //check if user already exists 
-        $query = "SELECT email FROM users WHERE email='$email'";
-        $result = mysqli_query($con, $query);
-        $count = mysqli_num_rows($result);
-                if($count == 0){
-                        $error = true;
-                        $email_error = "Provided email doesnot exist! Please register here <a id="a-color" href='register.php'>Register</a>";
-                }
-    }
+   
 ?>
 
 <!DOCTYPE html>
@@ -42,8 +29,8 @@ $error = false;
 				<div class="login-group">
 					<div class="form-group">
 						<label id="label-text-color" class="email">Email address</label>
-						<input type="email" name="email" placeholder="email@email.com" required class="email" value="<?php if($error) echo $email;?>" class="email">
-						<span class="text-danger"><?php if(isset($email_error))echo $email_error;?></span>	
+						<input type="email" name="email" placeholder="email@email.com" required class="email" class="email">
+				<!--		<span class="text-danger"> </span> -->	
 				</div>
 				</div>
 				<button id="bgcolor" type="submit" name="submit"  value="submit">Submit</button>
