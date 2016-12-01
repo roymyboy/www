92ae85adc49ca528ac72d9fc3fa29cb9a2dccc22
Utsav Roy <utroy@localhost.localdomain>
2016-12-01 11:21:17 -0700

    bg change

--- index.php
+++ index.php
@@ -37,6 +37,20 @@ if (isset($_POST['login'])) {
 
 <!DOCTYPE html>
 <html>
+<script>
+    var count = 1;
+    function setColor(btn, color) {
+        var property = document.getElementById(btn);
+        if (count == 0) {
+            property.style.backgroundColor = "#FFFFFF"
+            count = 1;
+        }
+        else {
+            property.style.backgroundColor = "#7FFF00"
+            count = 0;
+        }
+    }
+</script>
 <body>
 	<head>
 		<link rel = "stylesheet" type="text/css" href="style.css">
@@ -69,21 +83,6 @@ if (isset($_POST['login'])) {
 		</div>
 	</form>
    </div>
-<script>
-    var count = 1;
-    function setColor(btn, color) {
-        var property = document.getElementById(btn);
-        if (count == 0) {
-            property.style.backgroundColor = "#FFFFFF"
-            count = 1;
-        }
-        else {
-            property.style.backgroundColor = "#7FFF00"
-            count = 0;
-        }
-    }
-</script>
-
 </body>
 </html>
 <?php ob_end_flush(); ?>
