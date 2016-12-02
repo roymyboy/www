5640dd18fce94b111a207ce4e56f5c7cc446f9d0
Utsav Roy <utroy@localhost.localdomain>
2016-12-01 17:45:34 -0700

    bg change

--- forgotpw.php
+++ forgotpw.php
@@ -17,7 +17,7 @@ if(isset($_POST['email']))
                 $headers = "From: " . strip_tags($from) . "\r\n";
                 $headers .= "Reply-To: ". strip_tags($from) . "\r\n";
                 $headers .= "MIME-Version: 1.0\r\n";
-                $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+                $headers .= "Content-Type: text/html;
 
                 mail($to,$subject,$body,$headers);
 
