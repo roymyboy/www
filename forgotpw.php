c7663598478a83f433ba59b9cef99fc47eff7b6b
Utsav Roy <utroy@localhost.localdomain>
2016-12-04 09:41:59 -0700

    email

--- forgotpw.php
+++ forgotpw.php
@@ -1,7 +1,53 @@
 <?php
 session_start();
-include_once 'connect.php';
-
+include "connect.php";
+f (isset($_POST['username'])){
+	$username = $_POST['username'];
+	$query="SELECT * FROM `users` WHERE username='$username'";
+	$result   = mysqli_query($con, $query) or die(mysqli_error($con));
+	$count=mysqli_num_rows($result);
+	// If the count is equal to one, we will send message other wise display an error message.
+	if($count==1)
+	{
+		$rows=mysqli_fetch_array($result);
+		$pass  =  $rows['password'];//FETCHING PASS
+		//echo "your pass is ::".($pass)."";
+		$to = $rows['email'];
+		//echo "your email is ::".$email;
+		//Details for sending E-mail
+		$from = "Coding Cyber";
+		$url = "http://www.codingcyber.com/";
+		$body  =  "Coding Cyber password recovery Script
+		-----------------------------------------------
+		Url : $url;
+		email Details is : $to;
+		Here is your password  : $pass;
+		Sincerely,
+		Coding Cyber";
+		$from = "Your-email-address@domaindotcom";
+		$subject = "CodingCyber Password recovered";
+		$headers1 = "From: $from\n";
+		$headers1 .= "Content-type: text/html;charset=iso-8859-1\r\n";
+		$headers1 .= "X-Priority: 1\r\n";
+		$headers1 .= "X-MSMail-Priority: High\r\n";
+		$headers1 .= "X-Mailer: Just My Server\r\n";
+		$sentmail = mail ( $to, $subject, $body, $headers1 );
+	} else {
+	if ($_POST ['email'] != "") {
+	    $fmsg = "Not found your email in our database";
+		}
+		}
+	//If the message is sent successfully, display sucess message otherwise display an error message.
+	if($sentmail==1)
+	{
+		$smsg = "Your Password Has Been Sent To Your Email Address.";
+	}
+		else
+		{
+		if($_POST['email']!="")
+		$nmsg = "Cannot send password to your e-mail address.Problem with sending mail...";
+	}
+}
 
 ?>
 <html>
@@ -25,7 +71,7 @@ include_once 'connect.php';
 			</div>
 				<button id="bgcolor" type="submit" name="submit"  value="Forgotw">Submit</button>
 		</fieldset>
-			<span type="text-danger"><?php if(isset($message)) echo $message;?></span>
+			<span type="text-danger"><?php if(isset($msg)) echo $msg;?></span>
 			<p id="p-color">Already Registered? <a id="a-color"  href="index.php">login here</a></p>
 			<p id="p-color">New User? <a id="a-color" href="register.php">create new account</a></p>
 		</form>
