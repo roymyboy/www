f0fd74e68984fdf88ec4d5f8e910fa6217b1e49a
Utsav Roy <utroy@localhost.localdomain>
2016-12-05 17:47:55 -0700

    submit final

--- forgotpw.php
+++ forgotpw.php
@@ -10,14 +10,14 @@ include "connect.php";
 	<div id="bg"><img src="image/bg10.jpg" class="stretch" alt=""/></div>
 
 	<div class="container">
+			<!-- Main Form -->
 		<form role="form" method="post" name="forgotpw" action="<?php echo $_SERVER['PHP_SELF'];?>">
 		<fieldset>
 			<legend id="legendcolor">Forgot Password</legend>
-			<!-- Main Form -->
 	 		<div class="etc-login-form"><p id="p-color">When you fill in your registered email address, you will be sent instructions on how to reset your password.</p></div>
 				<div class="form-group">
 					<label id="label-text-color" for="email">Email address</label>
-					<input type="email" name="email" placeholder="email@email.com" required class="email"/>
+					<input type="email" name="email" placeholder="email@email.com" required class="email" />
 				</div>
 				<button id="bgcolor" type="submit" name="submit"  value="Forgotw">Submit</button>
 		</fieldset>
